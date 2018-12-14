fd55d5e9381f14b00e1bc1dbba1520050a762897
Robin O'Neill <robin.oneill@ao.com>
2018-12-14 10:47:48 +0000

    Fix build

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import '../styles/index.scss';
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
@@ -9,20 +9,28 @@ class App extends Component {
 
 	render() {
         return (
-            <div class="container mx-auto">
-                <h1 class="mb-4">Hello world</h1>
-                <p class="mb-3">The following image gets copied as a static asset using <a href="https://github.com/tiaanduplessis/parcel-plugin-asset-copier">parcel-plugin-asset-copier</a>.</p>
-                <p class="mb-3">So it can be used inside a React component without importing the assets.</p>
-                <img class="mt-4 block" src="./img/juice.jpg" />
+            <div className="container mx-auto px-3">
+                <h1 className="mb-4">Hello world</h1>
+
+                <div className="mb-3">
+                    <h2 className="mb-3">Styling</h2>
+                    <ul>
+                        <li>SCSS</li>
+                        <li>Post CSS</li>
+                        <li>Autoprefixer</li>
+                        <li><a href="https://tailwindcss.com/">Tailwind</a></li>
+                    </ul>
+                </div>
+
+                <div className="mb-3">
+                    <h2 className="mb-3">Images</h2>
+                    <p className="mb-3">The following image gets copied as a static asset using <a href="https://github.com/tiaanduplessis/parcel-plugin-asset-copier">parcel-plugin-asset-copier</a> so it can be used inside a React component without importing the assets.</p>
+                    <img className="mt-4 block w-64" src="./img/juice.jpg" />
+                </div>
             </div>
         );
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-// Hot Module Replacement
-if (module.hot) {
-	module.hot.accept();
-}
 
